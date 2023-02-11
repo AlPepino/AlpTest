1b7a2d8b162f93a5f18a1dfcbfaf35f390726711
David Psota <psotakdavid@gmail.com>
2023-02-11 14:52:31 +0100

    Přidejte soubory projektu.

---
+++ AlpTest/Controllers/HomeController.cs
@@ -0,0 +1,92 @@
+﻿using AlpTest.Models;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System.Diagnostics;
+using System.Text.Json.Serialization;
+
+namespace AlpTest.Controllers
+{
+    public class HomeController : Controller
+    {
+
+        private static DateTime TimeStampBtn { get; set; }
+        private static DateOnly DateCNB { get; set; }
+        public static float RateCZK { get; private set; }
+        public static Bitcoin CoinData = new Bitcoin();
+
+
+        public async Task<IActionResult> Index()
+        {
+            DateTime time = DateTime.Now;
+            if (DateCNB != DateOnly.FromDateTime(time))
+            {
+                DateCNB = DateOnly.FromDateTime(time);
+                using (var clientCNB = new HttpClient())
+                {
+                    using (var responseCNB = await clientCNB.GetAsync("https://www.cnb.cz/cs/financni_trhy/devizovy_trh/kurzy_devizoveho_trhu/denni_kurz.txt"))
+                    {
+                        string apiResponseCNB = await responseCNB.Content.ReadAsStringAsync();
+                        string[] rows = apiResponseCNB.Split("\n");
+                        foreach (string row in rows)
+                        {
+                            if (row.Contains("EUR"))
+                            {
+                                string[] euro = row.Split("|");
+                                RateCZK = float.Parse(euro[euro.Count() - 1]);
+                                break;
+                            }
+                        }
+                    }
+                }
+            }
+            var t = TimeStampBtn;
+            if ( t.AddSeconds(20) > time )
+            {
+                return View(CoinData);
+            } else
+            {
+                TimeStampBtn = time;
+                CoinData = new Bitcoin();
+                using (var client = new HttpClient())
+                {
+                    using (var response = await client.GetAsync("https://api.coindesk.com/v1/bpi/currentprice.json"))
+                    {
+                        string apiResponse = await response.Content.ReadAsStringAsync();
+                        CoinData = JsonConvert.DeserializeObject<Bitcoin>(apiResponse);
+                    }
+                }
+                CoinData.AddCZK(RateCZK);
+                return View(CoinData);
+            }
+        }
+        
+        [HttpGet]
+        public async Task<IActionResult> UpdateBitcoin()
+        {
+            DateTime time = DateTime.Now;
+            var t = TimeStampBtn;
+            if (t.AddSeconds(20) <= time)
+            {
+                TimeStampBtn = time;
+                CoinData = new Bitcoin();
+                using (var client = new HttpClient())
+                {
+                    using (var response = await client.GetAsync("https://api.coindesk.com/v1/bpi/currentprice.json"))
+                    {
+                        string apiResponse = await response.Content.ReadAsStringAsync();
+                        CoinData = JsonConvert.DeserializeObject<Bitcoin>(apiResponse);
+                    }
+                }
+                CoinData.AddCZK(RateCZK);
+            }
+            BpiSample sample = new BpiSample(CoinData.currentTime, CoinData.valueUSD, CoinData.valueGBP, CoinData.valueEUR, CoinData.valueCZK);
+            return Json(sample);
+        }        
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ AlpTest/Models/Bitcoin.cs
@@ -0,0 +1,118 @@
+﻿using NuGet.Common;
+using System.ComponentModel.DataAnnotations;
+using System.Globalization;
+using System.Reflection;
+using System.Xml.Linq;
+
+namespace AlpTest.Models
+{
+
+    public class BpiSample
+    {
+        public DateTime time { get; set; }
+        public float usd { get; set; }
+        public float gbp { get; set; }
+        public float eur { get; set; }
+        public float czk { get; set; }
+
+        public BpiSample(DateTime time, float usd, float gbp, float eur, float czk)
+        {
+            this.time = time;
+            this.usd = usd;
+            this.gbp = gbp;
+            this.eur = eur;
+            this.czk = czk;
+        }   
+    }
+
+
+    public class BpiElement 
+    { 
+        public string code { get; set; } = string.Empty;
+        public string symbol { get; set; } = string.Empty;
+        public string rate { get; set; } = string.Empty;
+        public string description { get; set; } = string.Empty;
+        public float rate_float { get; set; }
+    }
+
+
+    public class Bitcoin
+    {
+        public Dictionary<string, string> time = new Dictionary<string, string>();
+        public Dictionary<string, BpiElement> bpi = new Dictionary<string, BpiElement>();
+        public string chartName { get; set; } = string.Empty;
+
+
+        [Display(Name = "Time of last sample")]
+        public DateTime currentTime
+        {
+            get { return DateTime.Parse(time["updatedISO"]); }
+        }
+
+        [Display(Name = "Disclaimer")]
+        public string disclaimer { get; set; } = string.Empty;
+
+        [Display(Name = "USD")]
+        [DisplayFormat(DataFormatString = "{0:N}")]// $
+        public float valueUSD
+        {
+            get { return bpi["USD"].rate_float; }
+
+        }
+
+        [Display(Name = "GBP")]
+        [DisplayFormat(DataFormatString = "{0:N}")]// £
+        public float valueGBP
+        {
+            get { return bpi["GBP"].rate_float; }
+
+        }
+
+        [Display(Name = "EUR")]
+        [DisplayFormat(DataFormatString = "{0:N}")]// €
+        public float valueEUR
+        {
+            get { return bpi["EUR"].rate_float; }
+
+        }
+
+        [Display(Name = "CZK")]
+        [DisplayFormat(DataFormatString = "{0:N}")] // Kč
+        public float valueCZK
+        {
+            get { return bpi["CZK"].rate_float; }
+
+        }
+
+        private void Set(string key, BpiElement value)
+        {
+            if (bpi.ContainsKey(key))
+            {
+                bpi[key] = value;
+            }
+            else
+            {
+                bpi.Add(key, value);
+            }
+        }
+        private BpiElement Get(string key)
+        {
+            BpiElement result = new BpiElement();
+
+            if (bpi.ContainsKey(key))
+            {
+                result = bpi[key];
+            }
+
+            return result;
+        }
+
+        public void AddCZK(float rate)
+        {
+            float czk = valueEUR * rate;
+            BpiElement e = new BpiElement();
+            e.rate_float = czk;
+            Set("CZK", e);
+        }
+    }
+}
